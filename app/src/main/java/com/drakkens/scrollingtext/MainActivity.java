e4034cf4997a8366143c075360c2c38157f69f7f
Drakkens <72207118+Drakkens@users.noreply.github.com>
2021-10-22 20:22:09 +0200

    Initial commit

---
+++ app/src/main/java/com/drakkens/scrollingtext/MainActivity.java
@@ -0,0 +1,16 @@
+package com.drakkens.scrollingtext;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+import androidx.annotation.Nullable;
+
+public class MainActivity extends Activity {
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+    }
+}
